1db7151501409eec30488d87822ce70b3050e738
Himanshu Rathore <36191619+himanshu-802@users.noreply.github.com>
2020-07-21 21:36:19 +0530

    make it better

--- routes/admin.js
+++ routes/admin.js
@@ -3,65 +3,62 @@ const router = express.Router();
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
-
-// requiring product model
+//requiring product model
 let Product = require('../models/product');
-const product = require('../models/product');
-const { route } = require('./users');
 
-
-// check user is authenticated or not
+// Checks if user is authenticated
 function isAuthenticatedUser(req, res, next) {
-    if (req.isAuthenticated()) {
+    if(req.isAuthenticated()) {
         return next();
     }
-    req.flash('error_msg', 'Please login first to access the page');
+    req.flash('error_msg', 'Please Login first to access this page.')
     res.redirect('/login');
 }
 
 let browser;
 
-// scrape fuction
+//Scrape Function
 async function scrapeData(url, page) {
     try {
-        await page.goto(url, { waitUntil: 'load', timeout: 0 });
-        const html = await page.evaluate(() => document.body.innerHTML);
+        await page.goto(url, {waitUntil: 'load', timeout : 0});
+        const html = await page.evaluate(()=> document.body.innerHTML);
         const $ = await cheerio.load(html);
 
         let title = $("h1").attr('content');
         let price = $(".price-characteristic").attr("content");
-        if (!price) {
+
+        if(!price) {
             let dollars = $("#price > div > span.hide-content.display-inline-block-m > span > span.price-group.price-out-of-stock > span.price-characteristic").text();
             let cents = $("#price > div > span.hide-content.display-inline-block-m > span > span.price-group.price-out-of-stock > span.price-mantissa").text();
-            price = dollars + '.' + cents;
+            price = dollars+'.'+cents;
         }
 
         let seller = '';
         let checkSeller = $('.seller-name');
-        if (checkSeller) {
+        if(checkSeller) {
             seller = checkSeller.text();
         }
 
         let outOfStock = '';
         let checkOutOfStock = $('.prod-ProductOffer-oosMsg');
-        if (checkOutOfStock) {
+        if(checkOutOfStock) {
             outOfStock = checkOutOfStock.text();
         }
 
         let deliveryNotAvaiable = '';
         let checkDeliveryNotAvailable = $('.fulfillment-shipping-text');
-        if (checkDeliveryNotAvailable) {
+        if(checkDeliveryNotAvailable) {
             deliveryNotAvaiable = checkDeliveryNotAvailable.text();
         }
 
         let stock = '';
 
-        if (!(seller.includes('Walmart')) || outOfStock.includes('Out of Stock') ||
+        if(!(seller.includes('Walmart')) || outOfStock.includes('Out of Stock') || 
             deliveryNotAvaiable.includes('Delivery not available')) {
-            stock = 'Out of stock';
-        } else {
-            stock = 'In stock';
-        }
+                stock = 'Out of stock';
+            } else {
+                stock = 'In stock';
+            }
 
         return {
             title,
@@ -70,178 +67,169 @@ async function scrapeData(url, page) {
             url
         }
 
+    } catch (error) {
+        console.log(error);
     }
-    catch (error) {
-       // console.log(error);
-    }
-};
+}
 
-// ************GET ROUTES**************
+//GET routes starts here
 
-router.get('/', (req, res) => {
+router.get('/', (req,res)=> {
     res.render('./admin/index');
 });
 
-router.get('/dashboard', isAuthenticatedUser, (req, res) => {
+router.get('/dashboard', isAuthenticatedUser,(req,res)=> {
 
     Product.find({})
         .then(products => {
-            res.render('./admin/dashboard', { products: products });
-        });
-
+            res.render('./admin/dashboard', {products : products});
+    });
+    
 });
 
-router.get('/product/new', isAuthenticatedUser, async (req, res) => {
+
+router.get('/product/new', isAuthenticatedUser, async (req, res)=> {
     try {
         let url = req.query.search;
-        if (url) {
+        if(url) {
             browser = await puppeteer.launch({ args: ['--no-sandbox'] });
             const page = await browser.newPage();
-            let result = await scrapeData(url, page);
+            let result = await scrapeData(url,page);
 
             let productData = {
-                title: result.title,
-                price: '$' + result.price,
-                stock: result.stock,
-                productUrl: result.url
+                title : result.title,
+                price : '$'+result.price,
+                stock : result.stock,
+                productUrl : result.url
             };
-            res.render('./admin/newproduct', { productData: productData });
+            res.render('./admin/newproduct', {productData : productData});
             browser.close();
         } else {
             let productData = {
-                title: "",
-                price: "",
-                stock: "",
-                productUrl: ""
+                title : "",
+                price : "",
+                stock : "",
+                productUrl : ""
             };
-            res.render('./admin/newproduct', { productData: productData });
+            res.render('./admin/newproduct', {productData : productData});
         }
     } catch (error) {
-        req.flash('error_msg', 'ERROR: ' + error);
+        req.flash('error_msg', 'ERROR: '+error);
         res.redirect('/product/new');
     }
-
 });
 
-
-router.get('/product/search', isAuthenticatedUser, (req, res) => {
+router.get('/product/search', isAuthenticatedUser, (req,res)=> {
     let userSku = req.query.sku;
-    if (userSku) {
-        Product.findOne({ sku: userSku })
-            .then(function (product) {
-                if (!product) {
-                    req.flash('error_msg', 'Product does not exist in the databse');
-                    res.redirect('/product/search');
+    if(userSku) {
+        Product.findOne({sku : userSku})
+            .then(product => {
+                if(!product) {
+                    req.flash('error_msg', 'Product does not exist in the database.');
+                    return res.redirect('/product/search');
                 }
-                res.render('./admin/search', { productData: product });
+
+                res.render('./admin/search', {productData : product});
             })
-            .catch(function (err) {
-                req.flash('error_msg', 'ERROR: ' + err);
+            .catch(err => {
+                req.flash('error_msg', 'ERROR: '+err);
                 res.redirect('/product/new');
-            })
-
-    }
-    else {
-        res.render('./admin/search', { productData: '' });
+            });
+    } else {
+        res.render('./admin/search', {productData : ''});
     }
 });
 
-
-router.get('/products/instock', isAuthenticatedUser, (req, res) => {
-    Product.find({ newstock: "In stock" })
-        .then(function (product) {
-            console.log(product.url);
-            res.render('./admin/instock', { products: product });
+router.get('/products/instock', isAuthenticatedUser, (req,res)=> {
+    Product.find({newstock : "In stock"})
+        .then(products => {
+            res.render('./admin/instock', {products : products});
         })
-        .catch(function (err) {
-            req.flash('error_msg', 'ERR: ' + err);
-            res.redirect('/product/new');
-        });
-});
-router.get('/products/outofstock', isAuthenticatedUser, (req, res) => {
-    Product.find({ newstock: "Out of stock" })
-        .then(function (product) {
-            res.render('./admin/outofstock', { products: product });
-        })
-        .catch(function (err) {
-            req.flash('error_msg', 'ERR: ' + err);
-            res.redirect('/product/new');
+        .catch(err => {
+            req.flash('error_msg', 'ERROR: '+err);
+            res.redirect('/dashboard');
         });
 });
 
-router.get('/products/pricechanged', isAuthenticatedUser, (req, res) => {
+router.get('/products/outofstock', isAuthenticatedUser, (req,res)=> {
+    Product.find({newstock : "Out of stock"})
+        .then(products => {
+            res.render('./admin/outofstock', {products : products});
+        })
+        .catch(err => {
+            req.flash('error_msg', 'ERROR: '+err);
+            res.redirect('/dashboard');
+        });
+});
+
+router.get('/products/pricechanged', isAuthenticatedUser, (req,res)=> {
     Product.find({})
-        .then(function (product) {
-            res.render('./admin/pricechanged', ({ products: product }));
+        .then(products => {
+            res.render('./admin/pricechanged', {products : products});
         })
-        .catch(function (err) {
-            req.flash('error_msg', 'ERROR: ' + err);
+        .catch(err => {
+            req.flash('error_msg', 'ERROR: '+err);
             res.redirect('/dashboard');
-        })
-
+        });
 });
 
-router.get('/products/backinstock', isAuthenticatedUser, (req, res) => {
-    Product.find({ $and: [{ oldstock: 'Out of stock' }, { newstock: 'In stock' }] })
-        .then(function (product) {
-            res.render('./admin/backinstock', ({ products: product }));
+router.get('/products/backinstock', isAuthenticatedUser, (req,res)=> {
+    Product.find({$and: [{oldstock : 'Out of stock'},{newstock : 'In stock'}]})
+        .then(products => {
+            res.render('./admin/backinstock', {products : products});
         })
-        .catch(function (err) {
-            req.flash('error_msg', 'ERROR: ' + err);
+        .catch(err => {
+            req.flash('error_msg', 'ERROR: '+err);
             res.redirect('/dashboard');
-        })
-
+        });
 });
 
-router.get('/products/updated', isAuthenticatedUser, (req, res) => {
-    Product.find({ updatestatus: "Updated" })
-        .then(function (product) {
-            res.render('./admin/updatedproducts', { products: product });
+router.get('/products/updated', isAuthenticatedUser, (req,res)=> {
+    Product.find({updatestatus : "Updated"})
+        .then(products => {
+            res.render('./admin/updatedproducts', {products : products});
         })
-        .catch(function (err) {
-            req.flash('error_msg', 'ERRR: ' + err);
-            res.redirect('/dashboard');
-        })
-});
-
-router.get('/products/notupdated', isAuthenticatedUser, (req, res) => {
-    Product.find({ updatestatus: "Not Updated" })
-        .then(function (product) {
-            res.render('./admin/notupdatedproducts', { products: product });
-        })
-        .catch(function (err) {
-            req.flash('error_msg', 'ERRR: ' + err);
+        .catch(err => {
+            req.flash('error_msg', 'ERROR: '+err);
             res.redirect('/dashboard');
         });
 });
 
 
-router.get('/update', isAuthenticatedUser, (req, res) => {
-    res.render('./admin/update', { message: '' });
+router.get('/products/notupdated', isAuthenticatedUser, (req,res)=> {
+    Product.find({updatestatus : "Not Updated"})
+        .then(products => {
+            res.render('./admin/notupdatedproducts', {products : products});
+        })
+        .catch(err => {
+            req.flash('error_msg', 'ERROR: '+err);
+            res.redirect('/dashboard');
+        });
 });
 
+router.get('/update', isAuthenticatedUser, (req,res)=> {
+    res.render('./admin/update', {message : ''});
+});
+//POST routes starts here
 
-// ************POST Routes ****************************
-
-
-router.post('/product/new', isAuthenticatedUser, (req, res) => {
-    let { title, price, stock, url, sku } = req.body;
+router.post('/product/new', isAuthenticatedUser, (req,res)=> {
+    let {title, price, stock, url, sku} = req.body;
 
     let newProduct = {
-        title: title,
-        newprice: price,
-        oldprice: price,
-        newstock: stock,
-        oldstock: stock,
-        sku: sku,
-        company: "Walmart",
-        url: url,
-        updatestatus: "Updated"
+        title : title,
+        newprice : price,
+        oldprice : price,
+        newstock : stock,
+        oldstock : stock,
+        sku : sku,
+        company : "Walmart",
+        url : url,
+        updatestatus : "Updated"
     };
 
-    Product.findOne({ sku: sku })
+    Product.findOne({ sku : sku})
         .then(product => {
-            if (product) {
+            if(product) {
                 req.flash('error_msg', 'Product already exist in the database.');
                 return res.redirect('/product/new');
             }
@@ -253,12 +241,10 @@ router.post('/product/new', isAuthenticatedUser, (req, res) => {
                 })
         })
         .catch(err => {
-            req.flash('error_msg', 'ERROR: ' + err);
+            req.flash('error_msg', 'ERROR: '+err);
             res.redirect('/product/new');
         });
 });
-
-
 
 router.post('/update', isAuthenticatedUser, async(req, res)=>{
     try {
@@ -295,7 +281,6 @@ router.post('/update', isAuthenticatedUser, async(req, res)=>{
 });
 
 //DELETE routes starts here
-
 router.delete('/delete/product/:id', isAuthenticatedUser, (req, res)=> {
     let searchQuery = {_id : req.params.id};
 
@@ -310,8 +295,8 @@ router.delete('/delete/product/:id', isAuthenticatedUser, (req, res)=> {
         });
 });
 
-router.get('*',(req,res)=>{
-res.render('./admin/notfound');
+router.get('*', (req, res)=> {
+    res.render('./admin/notfound');
 });
 
 module.exports = router;
